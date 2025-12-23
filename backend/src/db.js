d93e88c968383522f50b41860c1da249bb00ccc5
AlexD20c <rihald07@gmail.com>
2025-12-23 01:19:29 -0300

    Actualizo db.js

--- backend/src/db.js
+++ backend/src/db.js
@@ -11,7 +11,7 @@ const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 5432,
+  port: 5432,
   ssl: {
     rejectUnauthorized: false,
   },
